24da5ecbf51d625e4ebf6c5b4b114261b10909cf
kishore-baskar <kishorebaskar4880@gmail.com>
2025-08-07 23:33:58 +0530

    feat: configure vite for gitpod

--- admin_portal/vite.config.js
+++ admin_portal/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allow access from the network
+    strictPort: true,
+    port: 5173, // Match the port in .gitpod.yml
+  },
 })
